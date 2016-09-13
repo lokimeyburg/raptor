346561a7c3dafd00d0807530c4a86c8018f0a1e4
lokimeyburg <loki@aristolabs.com>
2016-09-12 21:38:53 -0800

    integration tests working

--- spec/integration/channel_spec.rb
+++ spec/integration/channel_spec.rb
@@ -13,16 +13,31 @@ describe 'Integration:' do
         websocket.callback do
           case messages.length
           when 0
-            websocket.send_msg({ event: '/raptor/subscribe', data: { channel: 'MY_CHANNEL'} }.to_json)
+            websocket.send_msg({ event: '/raptor/subscribe', data: { channel: 'lobby' } }.to_json)
           when 1
-            Raptor['MY_CHANNEL'].trigger 'chat_message', { some: "data" }
+            Raptor['lobby'].trigger 'chat_message', { some: "data" }
           when 2
             EM.stop
           end
         end
       end # messages
       messages.should have_attributes connection_established: true, last_event: 'chat_message', last_data: { some: "data" }.to_json
-    end # it
+    end
+
+    it 'enforces one subcription per channel, per socket' do
+      messages = em_stream do |websocket, messages|
+        case messages.length
+        when 0
+          websocket.callback { websocket.send_msg({ event: '/raptor/subscribe', data: { channel: 'lobby'} }.to_json) }
+        when 1
+          websocket.send({ event: '/raptor/subscribe', data: { channel: 'lobby'} }.to_json)
+        when 2
+          EM.stop
+        end
+     end
+
+      messages.last.should == {"event"=>"/raptor/error", "data"=>{"code"=>nil, "message"=>"Existing subscription to lobby"}}
+    end
 
 
 
